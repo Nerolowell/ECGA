9a8ed0edf61ce663596a4d2fa3d15ba0bcaa395a
Nero Lowell <cuentaalexkiller@hotmail.com>
2019-11-04 00:26:30 -0700

    FIRST

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {ToastrModule} from 'ngx-toastr'
+import {CommonModule} from '@angular/common' 
+
+// firebase
+import {AngularFireModule} from 'angularfire2'
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {environment} from '../environments/environment';
+import{FormsModule} from '@angular/forms';
+//Componentes
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { ListaProductoComponent } from './componentes/productos/lista-producto/lista-producto.component';
+import { ProductoComponent } from './componentes/productos/producto/producto.component';
+//servicios
+import {productoService} from './servicios/producto.service';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductosComponent,
+    ListaProductoComponent,
+    ProductoComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-right'
+    }),
+    
+    
+    
+  ],
+  providers: [
+    productoService,
+  
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
